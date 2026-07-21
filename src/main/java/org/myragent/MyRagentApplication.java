fea95df2d96489806b462a9a77159e81c6abb371
Rick Li <79388294+rickmli@users.noreply.github.com>
2026-07-21 13:57:15 -0500

    first commit

---
+++ src/main/java/org/myragent/MyRagentApplication.java
@@ -0,0 +1,13 @@
+package org.myragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRagentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRagentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/myragent/MyRagentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRagentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
